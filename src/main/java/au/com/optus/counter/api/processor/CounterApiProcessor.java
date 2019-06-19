e4a45be12e129654cc5e56a7db344575f9edb75a
NimmyJoseph <josephnimmy494@gmail.com>
2019-06-20 02:25:11 +0530

    spring security update

--- src/main/java/au/com/optus/counter/api/processor/CounterApiProcessor.java
+++ src/main/java/au/com/optus/counter/api/processor/CounterApiProcessor.java
@@ -21,12 +21,13 @@ import au.com.optus.counter.api.models.SearchResponse;
 
 @Component
 public class CounterApiProcessor {
-	
+
 	@Value("${text.file}")
 	protected String textFile;
 
-	public SearchResponse stringCountProcess(SearchRequest request) throws IOException {
-		
+	public SearchResponse stringCountProcess(SearchRequest request)
+			throws IOException {
+
 		SearchResponse response = new SearchResponse();
 		ArrayList<HashMap<String, Integer>> txt = new ArrayList<HashMap<String, Integer>>();
 		for (String key : request.getSearchText()) {
@@ -35,71 +36,60 @@ public class CounterApiProcessor {
 			txt.add(map1);
 		}
 		response.setCount(txt);
-	    
+
 		return response;
 	}
 
 	public String topCountProcess(String val) throws IOException {
 		String returnStr = new String();
 		Integer top = Integer.parseInt(val);
-		/*LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<>();
-		 
-		getCount().entrySet()
-		    .stream()
-		    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-		    .forEachOrdered(x -> sortedMap.put(x.getKey(), x.getValue()));
-		Map<String, Integer> sortedMap = getCount();
-		int count = 1;
+		Map<String, Integer> map = getCount();
 
-		for (String key : sortedMap.keySet()) {
-			if (count <= top) {
-				returnStr = returnStr.concat(key+"|"+sortedMap.getOrDefault(key, 0)+"\n");
-			}
-			count++;
-		}*/
-Map<String, Integer> map = getCount();
-		
-		List<Map.Entry<String, Integer>> resultList = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());
-        Collections.sort(resultList, new Comparator<Map.Entry<String, Integer>>() {
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                return (o2.getValue() - o1.getValue());
-            }
-        });
-       
-        int count = 1;
+		List<Map.Entry<String, Integer>> resultList = new ArrayList<Map.Entry<String, Integer>>(
+				map.entrySet());
+		Collections.sort(resultList,
+				new Comparator<Map.Entry<String, Integer>>() {
+					public int compare(Map.Entry<String, Integer> o1,
+							Map.Entry<String, Integer> o2) {
+						return (o2.getValue() - o1.getValue());
+					}
+				});
+
+		int count = 1;
 
 		for (Entry<String, Integer> key : resultList) {
 			if (count <= top) {
-				returnStr = returnStr.concat(key.getKey()+"|"+key.getValue()+"\n");
+				returnStr = returnStr.concat(key.getKey() + "|"
+						+ key.getValue() + "\n");
 			}
 			count++;
 		}
-	    return returnStr;
+		return returnStr;
 	}
-	    
+
 	public Map<String, Integer> getCount() throws IOException {
-		File file = new File(getClass().getClassLoader().getResource(textFile).getFile());
-		 StringBuilder stringBuilder = new StringBuilder();
-	        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-	        String line;
-	        while ((line = bufferedReader.readLine()) != null) {
-	        	stringBuilder.append(line).append(" ");
-	        }
-	         
-	       
-	        Map<String, Integer> map = new HashMap<String, Integer>();  
-	        String sentence = stringBuilder.toString(); 
-	        StringTokenizer token = new StringTokenizer(sentence); 
-	        while (token.hasMoreTokens()) { 
-	            String words = token.nextToken(", ?.!:\"\"''\n").toLowerCase();  
-	            
-	            if (map.containsKey(words)) {
-	                int count = map.get(words);
-	                map.put(words, count + 1); 
-	            } else {
-	                map.put(words, 1); 
-	            }
-	        }
+		File file = new File(getClass().getClassLoader().getResource(textFile)
+				.getFile());
+		StringBuilder stringBuilder = new StringBuilder();
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+		String line;
+		while ((line = bufferedReader.readLine()) != null) {
+			stringBuilder.append(line).append(" ");
+		}
+
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		String sentence = stringBuilder.toString();
+		StringTokenizer token = new StringTokenizer(sentence);
+		while (token.hasMoreTokens()) {
+			String words = token.nextToken(", ?.!:\"\"''\n").toLowerCase();
+
+			if (map.containsKey(words)) {
+				int count = map.get(words);
+				map.put(words, count + 1);
+			} else {
+				map.put(words, 1);
+			}
+		}
 		return map;
 	}
 }
